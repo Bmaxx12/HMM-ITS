939199355a3d00f789166de0085b3064a1e44263
Bmaxx12 <ardanputra405@gmail.com>
2026-07-28 00:39:55 +0700

    Add Open Graph meta tags and Google Organization Schema

--- hmm-its/resources/views/components/layouts/app.blade.php
+++ hmm-its/resources/views/components/layouts/app.blade.php
@@ -12,6 +12,36 @@
     <link rel="shortcut icon" href="{{ asset('images/logo_hmm.png') }}">
     <link rel="apple-touch-icon" href="{{ asset('images/logo_hmm.png') }}">
 
+    {{-- Open Graph / Social Media Meta Tags --}}
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:title" content="HMM ITS - Himpunan Mahasiswa Mesin FT-IRS ITS">
+    <meta property="og:description" content="Website Resmi Himpunan Mahasiswa Mesin (HMM) FT-IRS Institut Teknologi Sepuluh Nopember Surabaya. Wadah eskalasi dan karya mahasiswa mesin ITS.">
+    <meta property="og:image" content="{{ asset('images/logo_hmm.png') }}">
+
+    {{-- Twitter Card Meta Tags --}}
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="HMM ITS - Himpunan Mahasiswa Mesin FT-IRS ITS">
+    <meta name="twitter:description" content="Website Resmi Himpunan Mahasiswa Mesin (HMM) FT-IRS Institut Teknologi Sepuluh Nopember Surabaya. Wadah eskalasi dan karya mahasiswa mesin ITS.">
+    <meta name="twitter:image" content="{{ asset('images/logo_hmm.png') }}">
+
+    {{-- Google Organization Schema (Structured Data) --}}
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "EducationalOrganization",
+      "name": "HMM ITS",
+      "alternateName": "Himpunan Mahasiswa Mesin FT-IRS ITS",
+      "url": "https://hmmits.com",
+      "logo": "https://hmmits.com/images/logo_hmm.png",
+      "sameAs": [
+        "https://www.instagram.com/hmm_its",
+        "https://www.its.ac.id/mesin"
+      ],
+      "description": "Website Resmi Himpunan Mahasiswa Mesin (HMM) FT-IRS Institut Teknologi Sepuluh Nopember Surabaya."
+    }
+    </script>
+
     {{-- Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
